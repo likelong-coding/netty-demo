75a200183ce82ebd40758a6d619d3e013f7a6e75
likelong <likelong123>
2022-12-26 10:28:33 +0800

    FileChannel transferTo 复制文件

---
+++ src/test/java/com/lkl/netty/test/CopyFileTest.java
@@ -0,0 +1,30 @@
+package com.lkl.netty.test;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * @author likelong
+ * @date 2022/12/26
+ */
+public class CopyFileTest {
+
+    public static void main(String[] args) {
+        try (
+                FileChannel from = FileChannel.open(Paths.get("1.jpg"), StandardOpenOption.READ);
+                FileChannel to = FileChannel.open(Paths.get("2.jpg"), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        ) {
+            long size = from.size();
+            long capacity = from.size();
+            // 分多次传输
+            while (capacity > 0) {
+                // transferTo返回值为传输了的字节数
+                capacity -= from.transferTo(size - capacity, capacity, to);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
