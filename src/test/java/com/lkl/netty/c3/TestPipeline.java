edab1fcee4220e8fa2256c60570dcba0c76a55e7
likelong <247907lkl>
2023-01-05 23:50:46 +0800

    Pipeline

---
+++ src/test/java/com/lkl/netty/c3/TestPipeline.java
@@ -0,0 +1,86 @@
+package com.lkl.netty.c3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author likelong
+ * @date 2023/1/5
+ */
+@Slf4j
+public class TestPipeline {
+
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+
+                        // 进站处理器 执行顺序从前(head)往后，出站处理器 执行顺序从后(tail)往前
+                        //1、通过 channel 获取 pipeline
+                        ChannelPipeline pipeline = ch.pipeline();
+
+                        // Inbound 进站处理器 一般是写入操作
+                        // 2、添加处理器 head -> h1 -> h2 -> h3 -> h4 -> h5 -> h6 -> tail
+                        // 调试方便给每个 handler 起个名字
+                        pipeline.addLast("h1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("1");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        pipeline.addLast("h2", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("2");
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+
+                        pipeline.addLast("h3", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log.debug("3");
+                                super.channelRead(ctx, msg);
+                                // 向 channel 中写数据
+                                ch.writeAndFlush(ctx.alloc().buffer().writeBytes("Server...".getBytes(StandardCharsets.UTF_8)));
+                            }
+                        });
+   
+                        // Outbound 出站处理器，一般是写操作 只有出站处理器向 channel 写入数据才会触发 出站处理器
+                        pipeline.addLast("h4", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("4");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+
+                        pipeline.addLast("h5", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("5");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+
+                        pipeline.addLast("h6", new ChannelOutboundHandlerAdapter() {
+                            @Override
+                            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                                log.debug("6");
+                                super.write(ctx, msg, promise);
+                            }
+                        });
+                    }
+                }).bind(8080);
+    }
+}
